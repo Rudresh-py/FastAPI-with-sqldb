7270eab2ce8b7b7b5576b59c34fee1efe6431ebc
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-06-08 14:51:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import engine, sessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db())):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already register")
+    return crud.create_user(db=db, user=user)
